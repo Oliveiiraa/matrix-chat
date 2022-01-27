df86aae4c8cbf0cf8b8f508ce88abbda5f21b4df
Gabriel Oliveira <gabrielh-oliveira@hotmail.com>
2022-01-26 23:10:24 -0300

    feat: add states and _app.js

---
+++ pages/chat.js
@@ -0,0 +1,7 @@
+export default function PageChat() {
+  return (
+    <>
+      <div>Página do chat</div>
+    </>
+  );
+}
--- pages/index.js
+++ pages/index.js
@@ -1,34 +1,7 @@
 import appConfig from '../config.json';
 import { Box, Button, Text, TextField, Image } from '@skynexui/components';
-
-function GlobalStyle() {
-  return (
-    <style global jsx>{`
-      * {
-        margin: 0;
-        padding: 0;
-        box-sizing: border-box;
-        list-style: none;
-      }
-      body {
-        font-family: 'Open Sans', sans-serif;
-      }
-      /* App fit Height */ 
-      html, body, #__next {
-        min-height: 100vh;
-        display: flex;
-        flex: 1;
-      }
-      #__next {
-        flex: 1;
-      }
-      #__next > * {
-        flex: 1;
-      }
-      /* ./App fit Height */ 
-    `}</style>
-  );
-}
+import { useState } from 'react';
+import { useRouter } from 'next/router';
 
 function Title(props) {
   const Tag = props.tag || "h1";
@@ -47,11 +20,11 @@ function Title(props) {
 }
 
 export default function PaginaInicial() {
-  const username = 'Oliveiiraa';
+  const [username, setUsername] = useState('');
+  const router = useRouter();
 
   return (
     <>
-      <GlobalStyle />
       <Box
         styleSheet={{
           display: 'flex', alignItems: 'center', justifyContent: 'center',
@@ -78,6 +51,10 @@ export default function PaginaInicial() {
           {/* Formulário */}
           <Box
             as="form"
+            onSubmit={(e) => {
+              e.preventDefault();
+
+            }}
             styleSheet={{
               display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center',
               width: { xs: '100%', sm: '50%' }, textAlign: 'center', marginBottom: '32px',
@@ -98,10 +75,16 @@ export default function PaginaInicial() {
                   backgroundColor: appConfig.theme.colors.neutrals[800],
                 },
               }}
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
             />
             <Button
               type='submit'
               label='Entrar'
+              onClick={(e) => {
+                setUsername(e.target.value)
+                router.push('/chat')
+              }}
               fullWidth
               buttonColors={{
                 contrastColor: appConfig.theme.colors.neutrals["000"],
